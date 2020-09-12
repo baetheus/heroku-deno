e776f2dd77419c681eed1dca75bb6988491a2348
Brandon Blaylock <brandon@null.pub>
2020-09-11 20:27:50 -0700

    feat: try some redis

--- main.ts
+++ main.ts
@@ -1,25 +1,45 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-// import { connect } from "https://denopkg.com/keroxp/deno-redis/mod.ts";
+import { urlParse } from "https://deno.land/x/url_parse/mod.ts";
+import { connect } from "https://denopkg.com/keroxp/deno-redis/mod.ts";
+
+const nil = <A>(a: A): a is NonNullable<A> => a !== undefined && a !== null;
+const ordinal = (i: number): string => {
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return i + "st";
+  }
+  if (j == 2 && k != 12) {
+    return i + "nd";
+  }
+  if (j == 3 && k != 13) {
+    return i + "rd";
+  }
+  return i + "th";
+};
 
 const PORT = Deno.env.get("PORT");
 const REDIS_URL = Deno.env.get("REDIS_URL");
 
-// const redis = await connect();
+if (nil(REDIS_URL)) {
+  console.error("Cannot start app without REDIS_URL");
+  Deno.exit(1);
+} else {
+  const redis_url = new URL((REDIS_URL as unknown) as string);
 
-const port = typeof PORT === "undefined" ? 3000 : parseInt(PORT, 10);
-const server = serve({ port });
-
-console.log(`http://localhost:${port}/`);
-
-for await (const req of server) {
-  req.respond({
-    body: JSON.stringify(
-      {
-        REDIS_URL,
-        PORT,
-      },
-      null,
-      2
-    ),
+  const redis = await connect({
+    ...redis_url,
   });
+
+  const port = typeof PORT === "undefined" ? 3000 : parseInt(PORT, 10);
+  const server = serve({ port });
+
+  console.log(`http://localhost:${port}/`);
+
+  for await (const req of server) {
+    const count = await redis.incr("COUNT");
+    req.respond({
+      body: `This is the ${ordinal(count)} view.`,
+    });
+  }
 }
