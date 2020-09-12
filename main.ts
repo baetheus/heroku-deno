798eb46c06a7612da8a8d280c72d7a6d0e2a4f16
Brandon Blaylock <brandon@null.pub>
2020-09-11 20:58:14 -0700

    fix: fork deno-redis for username

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-import { connect } from "https://denopkg.com/keroxp/deno-redis/mod.ts";
+import { connect } from "https://raw.githubusercontent.com/baetheus/deno-redis/master/mod.ts";
 
 const nil = <A>(a: A): a is NonNullable<A> => a === undefined && a === null;
 const ordinal = (i: number): string => {
@@ -26,12 +26,12 @@ if (nil(REDIS_URL)) {
 }
 
 const mucked_url = ((REDIS_URL as any) as string).replace(/^redis/, "http");
-const redis_url = new URL(mucked_url);
+const { hostname, port, username, password } = new URL(mucked_url);
 const serve_port = typeof PORT === "undefined" ? 3000 : parseInt(PORT, 10);
 
-console.log({ redis_url, serve_port });
+console.log({ redis: { hostname, port, username, password }, serve_port });
 
-const redis = await connect(redis_url);
+const redis = await connect({ hostname, port, username, password });
 const server = serve({ port: serve_port });
 
 for await (const req of server) {
