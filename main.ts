67641f2e147d637af9982261d1b69dfcd0f25a07
Brandon Blaylock <brandon@null.pub>
2020-09-11 20:14:13 -0700

    feat: do some snooping

--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
 import { serve } from "https://deno.land/std/http/server.ts";
+// import { connect } from "https://denopkg.com/keroxp/deno-redis/mod.ts";
 
 const PORT = Deno.env.get("PORT");
+const REDIS_URL = Deno.env.get("REDIS_URL");
+
+// const redis = await connect();
+
 const port = typeof PORT === "undefined" ? 3000 : parseInt(PORT, 10);
 const server = serve({ port });
 
 console.log(`http://localhost:${port}/`);
 
 for await (const req of server) {
-  req.respond({ body: "Hello World\n" });
+  req.respond({
+    body: JSON.stringify(
+      {
+        REDIS_URL,
+        PORT,
+      },
+      null,
+      2
+    ),
+  });
 }
