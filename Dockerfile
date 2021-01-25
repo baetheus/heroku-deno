85fe21afec639d3fa0f6528fbb4b2af445ac81b7
Brandon Blaylock <brandon@null.pub>
2021-01-24 20:56:31 -0800

    feat: stuff?

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM hayd/alpine-deno:1.5.0
 
-EXPOSE $PORT
-EXPOSE $REDIS_URL
-
 WORKDIR /app
 
 ADD . .
--- main.ts
+++ main.ts
@@ -1,59 +1,36 @@
 import { serve } from "https://deno.land/std@0.71.0/http/mod.ts";
-import { connect } from "https://deno.land/x/redis@v0.13.0/mod.ts";
 
 import { use } from "https://deno.land/x/sedate@v0.0.2/deno_handler.ts";
 import * as S from "https://deno.land/x/sedate@v0.0.2/sedate.ts";
 
 import { pipe } from "https://deno.land/x/hkts@v0.0.30/fns.ts";
-import * as D from 'https://deno.land/x/hkts@v0.0.30/decoder.ts';
-import * as E from 'https://deno.land/x/hkts@v0.0.30/either.ts';
 
-import { html } from 'https://deno.land/x/html/mod.ts';
-
-/**
- * Environmental Settings Decoder
- */
-const Settings = D.type({
-  PORT: D.string,
-  REDIS_URL: D.string,
-});
-
-/**
- * Parsed Environmental Settings
- */
-const { REDIS_URL, PORT } = pipe(
-  Settings.decode(Deno.env.toObject()),
-  E.getOrElse(() => ({ PORT: "5000", REDIS_URL: "redis://localhost:6379"}))
-);
-
-const redis_url = new URL(REDIS_URL.replace(/^redis/, "http"));
-const serve_port = typeof PORT === "undefined" ? 3000 : parseInt(PORT, 10);
-
-/**
- * Redis Connection
- */
-const redis = await connect({ hostname: redis_url.hostname, port: redis_url.port });
-await redis.auth(redis_url.password);
+import { html } from "https://deno.land/x/html/mod.ts";
 
 /**
  * Http Server
  */
-const server = serve({ port: serve_port });
+const PORT = Deno.env.get("PORT") ?? Deno.exit(1);
+const port = parseInt(PORT, 10);
+
+const server = serve({ port });
+
+let count = 0;
 
 /**
  * Template
  */
 const page = (count: string | number) => {
   return html`<html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>heroku-deno</title>
+    </head>
 
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>heroku-deno</title>
-</head>
-
-<body><h1>Hello, you are person number ${count}</h1></body>
-
-</html>`
+    <body>
+      <h1>Hello, you are person number ${count}</h1>
+    </body>
+  </html>`;
 };
 
 /**
@@ -63,8 +40,7 @@ const rootHandler = pipe(
   S.status(S.Status.OK),
   S.ichain(() => S.header("Content-Type", "text/html; charset=UTF-8")),
   S.ichain(() => S.closeHeaders()),
-  S.ichain(() => S.rightTask(() => redis.incr("COUNT"))),
-  S.ichain((count) => S.send(page(count))),
+  S.ichain(() => S.send(page(++count))),
   S.orElse(S.send)
 );
 
